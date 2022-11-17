26ab336a76c9c582bf2098a40e5444a137c05dbf
Chris Jobin <c.r.jobin@gmail.com>
2022-11-16 21:31:19 -0600

    Started on Form Components and Model

---
+++ PassApp.Web/Components/Form/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassApp.Web.Components.Form
+{
+    public class FormModel
+    {
+        public string? Title { get; set; }
+        public string? Link { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ PassApp.Web/Components/Form/ItemForm.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassApp.Web.Components.Form
+{
+    public partial class ItemForm
+    {
+
+    }
+}
