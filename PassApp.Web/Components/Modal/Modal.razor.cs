cfa8c456d4c80ec8ccc088d4c3953fb6cca90a87
Chris Jobin <c.r.jobin@gmail.com>
2022-11-16 19:43:40 -0600

    Added Modal Component

---
+++ PassApp.Web/Components/Modal/Modal.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassApp.Web.Components.Modal
+{
+    public partial class Modal
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public string Size { get; set; } = "xl";
+
+        public bool Show { get; set; }
+
+        public void Open()
+        {
+            Show = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            Show = false;
+            StateHasChanged();
+        }
+    }
+}
