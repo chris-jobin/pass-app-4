ec205c09863d61fec0a716b215b0f42f5ee419df
Chris Jobin <c.r.jobin@gmail.com>
2022-12-21 22:29:40 -0600

    Ensuring the creation of the database

--- PassApp.Data/PassAppContext.cs
+++ PassApp.Data/PassAppContext.cs
@@ -5,8 +5,15 @@ namespace PassApp.Data
 {
     public partial class PassAppContext : DbContext
     {
+        private static bool Created { get; set; }
+
         public PassAppContext(DbContextOptions<PassAppContext> options) : base(options) 
         {
+            if (!Created)
+            {
+                Database.EnsureCreated();
+                Created = true;
+            }
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
