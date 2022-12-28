0cf7ca79c665cd651af74d376f933beb3cf370b5
Chris Jobin <c.r.jobin@gmail.com>
2022-12-28 16:30:51 -0600

    Fixed some warnings

--- PassApp.Data/Models/User.cs
+++ PassApp.Data/Models/User.cs
@@ -9,7 +9,7 @@ namespace PassApp.Data.Models
 {
     public class User : Keyable
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
     }
 }
